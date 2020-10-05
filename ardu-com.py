0e0ad4b36a211becef18d507694316dd840b13d0
Aron <ahugoon@gmail.com>
2020-10-06 00:59:30 +0200

    Draft for socket thread

--- ardu-com.py
+++ ardu-com.py
@@ -1,10 +1,51 @@
 import time
 from Controller import Arduino_Controller
+import socket
+import threading
+
+class Server(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self, name="Server", daemon=True)
+        self._header = 64
+        self._port = 1337
+        self._server_ip = '0.0.0.0'
+        self._addr = (self._server_ip, self._port)
+        self._format = 'utf-8'
+        self.daemon = True
+
+    def handle_client(self, conn, addr):
+        print(f'\n[NEW CONNECTION] {addr} connected.')
+
+        connected = True
+        while connected:
+            msg_length = conn.recv(self._header).decode(self._format)
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(self._format)
+            print(f'[{addr}] {msg}')
+
+    def run(self):
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _socket.bind(self._addr)
+        _socket.listen()
+        print(f'\n[LISTENING] Server is listening on {self._addr}')
+        while True:
+            conn, addr = _socket.accept()
+            t = threading.Thread(target=self.handle_client, args=(conn, addr))
+            t.daemon = True
+            t.start()
 
 _controller = Arduino_Controller()
 _controller.start()
 
-while True:
-    _controller.sendMessage(input("Message:"))
+_server = Server()
+_server.start()
 
-_controller.stop()
+while True:
+    try:
+        _controller.sendMessage(input("Message:"))
+    except KeyboardInterrupt:
+        print('\n[KeyboardInterrupt] Closing connections and subprocesses.')
+        _controller.stop()
+        _server._stop()
+        print('Bye')
+        break
