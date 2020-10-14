85551f7f74ae60605a20a36dc198ac6a1a73411d
Aron <ahugoon@gmail.com>
2020-10-15 00:18:29 +0200

    Moved javascript into static folder

---
+++ web/control/static/control/main.js
@@ -0,0 +1,47 @@
+function openConnection() {
+    if (typeof socket !== 'undefined') {
+        if (socket.readyState != socket.CLOSED) {
+            alert('Already connected/connecting..')
+            return
+        }
+    }
+
+    socket = new WebSocket('ws://' + window.location.host + ':8080');
+
+    socket.addEventListener('open', function (event) {
+        console.log('[CONNECTED] Connected to server: ' + socket.url);
+        document.getElementsByTagName('h1')[0].style='color: green;'
+    });
+    
+    socket.addEventListener('close', function (event){
+        console.log('[CONNECTION CLOSED] The connection was closed: ' + socket.url)
+        document.getElementsByTagName('h1')[0].style='color: red;'
+    });
+
+    socket.addEventListener('message', function (event) {
+        console.log('[RECEIVED] Message from server ', event.data);
+    });
+
+    socket.onerror=function() {
+        alert('Could not connect..\nIs the server running?');
+    }
+}
+
+function closeConnection() {
+    if (typeof socket !== 'undefined') {
+        socket.close()
+    }
+    else {
+        alert('No socket found..')
+    }
+}
+function sendDirectCommand(cmd) {
+    if (typeof socket !== 'undefined' && socket.readyState == socket.OPEN) {
+        cmd = '{"direct-command":"' + cmd + '"}'
+        socket.send(cmd);
+        console.log('[SENT] Sent to server: ' + cmd);
+    }
+    else {
+        alert('No active connection..')
+    }
+}
