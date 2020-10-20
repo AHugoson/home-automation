2bdc7b8045b7100f02b16f7bf5cb3dd14e6eadc8
Aron <ahugoon@gmail.com>
2020-10-20 04:05:06 +0200

    Display alarms on control page

--- web/control/views.py
+++ web/control/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Alarm
 
 def home(request):
-    return render(request, 'control/home.html')
+    context = {
+        'alarms': Alarm.objects.all()
+    }
+    return render(request, 'control/home.html', context)
