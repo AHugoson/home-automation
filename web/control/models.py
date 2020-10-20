53127c330ad4024368955dd6cc95695e8077b9d1
Aron <ahugoon@gmail.com>
2020-10-20 03:59:57 +0200

    Add database model for alarms

--- web/control/models.py
+++ web/control/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Alarm(models.Model):
+    name = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    days = models.IntegerField(default=0)
+    time = models.TimeField()
+    command = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
