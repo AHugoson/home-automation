bc104e34bb18b4a12f953a7bcd0a674b8a2a0b48
Aron <ahugoon@gmail.com>
2020-10-21 23:54:15 +0200

    Get alarms from database and schedule to run

--- core.py
+++ core.py
@@ -1,7 +1,9 @@
 import asyncio
 import json
 import logging
+import schedule
 import time
+import sqlite3
 
 import websockets
 
@@ -16,8 +18,43 @@ _controller.start()
 _server = TCP_Server()
 _server.start()
 
+db_conn = sqlite3.connect('web/db.sqlite3')
+db_cursor = db_conn.cursor()
+
 connected = set()
 websocket_addr = ('0.0.0.0', 8080)
+
+async def updateActiveAlarms():
+    old_alarms = []
+    while True:
+        logging.debug("Fetching alarms from database")
+        db_cursor.execute("SELECT name, time, days, command FROM control_alarm WHERE active=1")
+        alarms = db_cursor.fetchall()
+        if alarms != old_alarms:
+            schedule.clear('alarm')
+            logging.info("Alarms updated in database, creating new schedule...")
+            for alarm in alarms:
+                logging.info(f"[{alarm[0]}] will go off at [{alarm[1]}] at days:[{alarm[2]}] and execute [{alarm[3].split('&&')}]")
+                scheduleAlarms(alarm[0], alarm[1], alarm[2], alarm[3].split('&&'), tag='alarm')
+            old_alarms = alarms
+            print(schedule.jobs)
+        schedule.run_pending()
+        await asyncio.sleep(1)
+
+def scheduleAlarms(name:str, time:str, days:int, commands:list, tag=''):
+    if days == 0:
+        schedule.every().day.at(time).do(sendMessages, commands).tag(name, tag)
+
+def sendMessages(commands:list):
+    for cmd in commands:
+        _controller.sendMessage(cmd)
+        time.sleep(1)
+
+async def handleCommandsTCP():
+    while True:
+        if _server._received.__contains__("direct-command"):
+            _controller.sendMessage(_server._received.pop("direct-command"))
+        await asyncio.sleep(0.01)
 
 async def handleClient(websocket:websockets.WebSocketServerProtocol, path):
     _addr = websocket.remote_address
@@ -42,13 +79,13 @@ async def handleClient(websocket:websockets.WebSocketServerProtocol, path):
         logging.info(f'Active connections: {len(connected)}')
 
 start_server = websockets.serve(handleClient, *websocket_addr)
+# Add all coroutines to the event loop
+loop = asyncio.get_event_loop()
 
-asyncio.get_event_loop().run_until_complete(start_server)
+loop.create_task(handleCommandsTCP())
+loop.create_task(updateActiveAlarms())
+
+loop.run_until_complete(start_server)
 logging.info(f'[LISTENING] WebSocket Server is listening on {websocket_addr}')
+
 asyncio.get_event_loop().run_forever()
-
-while True:
-    if _server._received.__contains__("direct-command"):
-        _controller.sendMessage(_server._received.pop("direct-command"))
-
-    time.sleep(0.01)
