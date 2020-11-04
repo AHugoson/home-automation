01b1b08d776fb884c33a75382351de3b1947ed12
Aron <ahugoon@gmail.com>
2020-11-04 23:03:19 +0100

    Added @react-three/drei, and some boxes..

--- app/src/pages/ThreeD.jsx
+++ app/src/pages/ThreeD.jsx
@@ -1,11 +1,10 @@
-import React, { Component, Suspense, useState, useRef } from "react";
+import React, { Component, useState, useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
-import { Canvas, useFrame, extend, useThree, useLoader } from "react-three-fiber";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
+import { MeshWobbleMaterial, Sky } from "@react-three/drei";
 import { useSpring, a } from "react-spring/three";
 import "../css/ThreeD.css";
-import diamond_holder from "../models/diamond_holder.gltf";
 
 THREE.Object3D.DefaultUp = new THREE.Vector3(0, 0, 1);
 
@@ -40,26 +39,26 @@ const Plane = () => (
   </mesh>
 );
 
-const Box = () => {
+const Box = (args) => {
   const meshRef = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
   const props = useSpring({
     scale: active ? [1, 1, 0.2] : [1, 1, 1],
     color: hovered ? "#00ff40" : "#00a030",
-    pos: active ? [0, 0, 0.2] : [0, 0, Math.sqrt(3)],
   });
   useFrame(() => {
     meshRef.current.rotation.x = active
       ? 0
       : meshRef.current.rotation.x + 0.006;
-    meshRef.current.rotation.y = active ? 0 : meshRef.current.rotation.y + 0.002;
+    meshRef.current.rotation.y = active
+      ? 0
+      : meshRef.current.rotation.y + 0.002;
     meshRef.current.rotation.z += 0.004;
   });
 
   return (
     <a.mesh
-      position={props.pos}
       ref={meshRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => {
@@ -67,31 +66,44 @@ const Box = () => {
       }}
       onPointerDown={() => setActive(!active)}
       scale={props.scale}
+      {...args}
     >
-      <a.boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
-      <a.meshBasicMaterial wireframe attach="material" color={props.color} />
+      <a.boxBufferGeometry attach="geometry" args={[0.5, 0.5, 0.5]} />
+      <MeshWobbleMaterial
+        color="rgb(0,255,0)"
+        attach="material"
+        factor={5} // Strength, 0 disables the effect (default=1)
+        speed={2} // Speed (default=1)
+      />
     </a.mesh>
   );
 };
-
-function Model() {
-  const { nodes } = useLoader(GLTFLoader, diamond_holder);
-  return (
-      <mesh visible scale={[50,50,50]} geometry={nodes.scene}>
-      </mesh>
-  );
-}
 
 class ThreeDPage extends Component {
   state = {
     pathname: this.props.location.pathname,
   };
 
+  generateBoxes() {
+    var dimensions = [10, 10, 2];
+    var boxes = [];
+    for (var x = -(dimensions[0]-1)/2; x < dimensions[0]/2; x++) {
+      for (var y = -(dimensions[1]-1)/2; y < dimensions[1]/2; y++) {
+        for (var z = 1; z <= dimensions[2]; z++) {
+          boxes.push([x, y, z]);
+        }
+      }
+    }
+    return boxes.map((pos, i) => <Box position={pos} key={i}></Box>);
+  }
+
   render() {
     return (
       <div className="content">
         <Canvas style={{ height: "100vh" }} camera={{ position: [0, 0, 6] }}>
-          <spotLight />
+          {/* <spotLight position={[0, 0, 5]} /> */}
+          <pointLight position={[0, 0, 5]} />
+          <ambientLight intensity={0.2} />
           <Controls />
           <polarGridHelper
             position={[0, 0, 0.01]}
@@ -101,10 +113,8 @@ class ThreeDPage extends Component {
           {/* <gridHelper rotation={[0, 0, Math.PI / 2]} args={[60, 60]} /> */}
           {/* <gridHelper args={[60, 60]} /> */}
           <Plane />
-          <Box />
-          <Suspense fallback={null}>
-            <Model />
-          </Suspense>
+
+          {this.generateBoxes()}
           {/* <fog attach="fog" args={["blue", 5, 50]} /> */}
         </Canvas>
       </div>
